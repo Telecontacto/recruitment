25be0b6c3d173e29ab9643347841ddd934b1443a
Israel Berrios <iberrios@telecontacto.com>
2025-03-17 08:37:33 -0400

    refactor AcmeLogo component; update logo source to use external URL

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,7 +3,7 @@ export default function AcmeLogo() {
   return (
     <div className="relative">
       <img
-        src="/tc-mini-logo-white.png"
+        src="https://reports.telecontacto.com/images/tc-mini-logo-white.png"
         width={500}
         height={760}
         alt="Telecontacto Logo"
